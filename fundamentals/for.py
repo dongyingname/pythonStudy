e796c354a64dc107dec3dbbb05d296f074b48186
Ying Dong <dongyingname@yahoo.com>
2020-01-30 21:40:32 -0800

    for loop using range()

--- fundamentals/for.py
+++ fundamentals/for.py
@@ -4,3 +4,12 @@ for w in words:
     print(w,len(w))
 
 #loop on a copy of the original
+
+#iterate over a sequence of numbers
+for i in range(5):
+    print(i)
+
+a = ['Mary','had','a', 'little','lamb']
+for i in range(len(a)):
+    print(i, a[i])
+    
