3d7b27cee81a7cfe30baa159c54c6d67f32f8053
Ying Dong <dongyingname@yahoo.com>
2020-01-30 21:54:06 -0800

    continue statment

--- fundamentals/breakOrContinue.py
+++ fundamentals/breakOrContinue.py
@@ -5,3 +5,9 @@ for n in range(2,10):
             break
         else:
             print(n, 'is a prime number')
+            
+for num in range(2,10):
+    if num % 2 ==0:
+        print("Found an even number", num)
+        continue
+    print("found a number", num)
