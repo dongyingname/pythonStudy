056841168df8cc7829faa8b6e07225dff7c545b4
Ying Dong <dongyingname@yahoo.com>
2020-02-12 13:30:03 +1000

    Update specialParameters.py

--- fundamentals/function/specialParameters.py
+++ fundamentals/function/specialParameters.py
@@ -1,11 +1,13 @@
 def standard_arg(arg):
     print(arg)
 
-def pos_only_arg(arg, /):
+def pos_only_arg(arg, /): #postional only
     print(arg)
 
-def kwd_only_arg(*, arg):
+def kwd_only_arg(*, arg): #keyword only
     print(arg)
 
 def combined_example(pos_only, /, standard, *, kwd_only):
     print(pos_only, standard, kwd_only)
+
+combined_example(1, standard=2, kwd_only=3)
